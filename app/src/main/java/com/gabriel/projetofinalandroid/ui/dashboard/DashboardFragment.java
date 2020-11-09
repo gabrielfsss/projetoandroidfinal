7fb502822adb2daed163211451e81ac9b0be0a22
Gabriel Freire <gabrielfreiredesouza.souza@gmail.com>
2020-11-09 15:50:26 -0700

    projeto final

--- app/src/main/java/com/gabriel/projetofinalandroid/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/gabriel/projetofinalandroid/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,13 @@
 package com.gabriel.projetofinalandroid.ui.dashboard;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,14 +16,35 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.gabriel.projetofinalandroid.MainActivity;
+import com.gabriel.projetofinalandroid.R;
+import com.google.firebase.auth.FirebaseAuth;
+
 import com.gabriel.projetofinalandroid.R;
 
 public class DashboardFragment extends Fragment {
+
+    private Button btnlogout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        Button btn2 = (Button) root.findViewById(R.id.btnlogout);
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut();
+
+                Intent i = new Intent(getActivity(), MainActivity.class);
+                startActivity(i);
+
+                getActivity().finish();
+            }
+        });
 
         return root;
     }
