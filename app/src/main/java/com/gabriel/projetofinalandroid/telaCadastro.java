5e49a0ba87ada0fc23cc610e59aacf38ab594e3e
Gabriel Freire <gabrielfreiredesouza.souza@gmail.com>
2020-11-09 13:43:04 -0700

    projeto final

--- app/src/main/java/com/gabriel/projetofinalandroid/telaCadastro.java
+++ app/src/main/java/com/gabriel/projetofinalandroid/telaCadastro.java
@@ -30,4 +30,38 @@ public class telaCadastro extends AppCompatActivity {
         txtEmail = findViewById(R.id.txtEmail);
         txtSenha = findViewById(R.id.txtSenha);
     }
+
+    public void cadastrarUsuario(View view){
+        final String nome = txtNome.getText().toString().trim();
+        final String email= txtEmail.getText().toString().trim();
+        final String senha = txtSenha.getText().toString().trim();
+
+        if (!nome.isEmpty() && !email.isEmpty() && !senha.isEmpty()){
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Usuario usuario = new Usuario();
+                        usuario.setId(task.getResult().getUser().getUid());
+                        usuario.setNome(nome);
+                        usuario.setEmail(email);
+                        usuario.setSenha(senha);
+                        FirebaseFirestore.getInstance().collection("usuarios").document(usuario.getId()).set(usuario).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(getApplicationContext(), "Realize Seu Login...", Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                        });
+                    }else Toast.makeText(getApplicationContext(), "Por favor tente novamente...", Toast.LENGTH_LONG).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("Error", "Cadastrar Auth: "+e.getMessage(), e);
+                    Toast.makeText(getApplicationContext(), "Por favor tente novamente...", Toast.LENGTH_LONG).show();
+                }
+            });
+        }else Toast.makeText(getApplicationContext(), "Informe todos os campos!", Toast.LENGTH_LONG).show();
+    }
 }
