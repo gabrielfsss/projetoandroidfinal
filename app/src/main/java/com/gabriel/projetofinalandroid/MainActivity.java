b2a2d8ed65a29021102d9433dde376c454b176a8
Gabriel Freire <gabrielfreiredesouza.souza@gmail.com>
2020-11-09 15:13:39 -0700

    projeto final

--- app/src/main/java/com/gabriel/projetofinalandroid/MainActivity.java
+++ app/src/main/java/com/gabriel/projetofinalandroid/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+
+        if (FirebaseAuth.getInstance().getUid()!=null){
+            Intent intent = new Intent(getApplicationContext(), telasApp.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 
     public void Entrar (View v){
