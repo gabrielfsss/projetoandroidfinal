1c6d2948af156689472c4ac95ca08116a9e4d81f
Gabriel Freire <gabrielfreiredesouza.souza@gmail.com>
2020-11-09 19:51:18 -0700

    projeto final tela chat atualizada

--- app/src/main/java/com/gabriel/projetofinalandroid/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/gabriel/projetofinalandroid/ui/notifications/NotificationsFragment.java
@@ -4,23 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.gabriel.projetofinalandroid.R;
+import com.gabriel.projetofinalandroid.Usuario;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.xwray.groupie.GroupAdapter;
 
 public class NotificationsFragment extends Fragment {
 
+    private EditText txtMessageChat;
+    private FloatingActionButton btnSendMessage;
+    private Usuario me;
+    private GroupAdapter adapter;
 
-
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        txtMessageChat = root.findViewById(R.id.txtMessageChat);
+        btnSendMessage = root.findViewById(R.id.btnSendChat);
+        RecyclerView rcView = root.findViewById(R.id.rcView);
+
+        adapter = new GroupAdapter();
+        rcView.setLayoutManager(new LinearLayoutManager(getContext()));
+        rcView.setAdapter(adapter);
+
+        FirebaseFirestore.getInstance().collection("/usuarios")
+                .document(FirebaseAuth.getInstance().getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Usuario a = documentSnapshot.toObject(Usuario.class);
+                        fetchMessages();
+                    }
+                });
 
         return root;
     }
